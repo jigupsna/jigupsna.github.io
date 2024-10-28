eada7d9bed9c2d7c525bace5f7a462de532adc2d
jigupsna <jigneshpatel@Jigneshs-MacBook-Pro.local>
2024-10-28 18:50:25 +0530

    dtchs

--- dashboard.js
+++ dashboard.js
@@ -16,10 +16,10 @@ async function subs()
                         <th>Total Value (Aug 2024)</th>
                         <th>Next Cycle</th>
                     </tr>
-                    <tr></tr><td>Zion Taxi Services</td><td>29 November 2023</td><td>Yearly</td><td>50 EUR/month</td><td>400 EUR</td><td>29 November 2024</td></tr>
-                    <tr></tr><td>Harmain Rentals</td><td>13 December 2023</td><td>Yearly</td><td>65 EUR/month</td><td>520 EUR</td><td>13 December 2024</td></tr>
-                    <tr></tr><td>Skaha Car Rentals</td><td>20 December 2023</td><td>Yearly</td><td>40 EUR/month</td><td>320 EUR</td><td>20 December 2024</td></tr>
-                    <tr></tr><td>KAP Car rental Lagos</td><td>28 December 2023</td><td>Yearly</td><td>45 EUR/month</td><td>360 EUR</td><td>28 December 2024</td></tr>
+                    <tr></tr><td>Zion Taxi Services</td><td>29 November 2023</td><td>Yearly</td><td>50 EUR/month</td><td>450 EUR</td><td>29 November 2024</td></tr>
+                    <tr></tr><td>Harmain Rentals</td><td>13 December 2023</td><td>Yearly</td><td>65 EUR/month</td><td>585 EUR</td><td>13 December 2024</td></tr>
+                    <tr></tr><td>Skaha Car Rentals</td><td>20 December 2023</td><td>Yearly</td><td>40 EUR/month</td><td>360 EUR</td><td>20 December 2024</td></tr>
+                    <tr></tr><td>KAP Car rental Lagos</td><td>28 December 2023</td><td>Yearly</td><td>45 EUR/month</td><td>405 EUR</td><td>28 December 2024</td></tr>
             </table>
     </div>
     <br>
@@ -60,6 +60,8 @@ async function balance()
                     <tr></tr><td>Clearing Initiated to Connected Account on 02 September 2024</td><td>510.00 EUR (-)</td></tr>
                     <tr></tr><td>Verification Passed on 24 September 2024 [GMT+05:30] for 510.00 EUR settlement</td><td>[[]]</td></tr>
                     <tr></tr><td>Settlement Initiated on 25 September 2024 [GMT+05:30] for 510.00 EUR to [HDFC *****308**6228 JIG**** PA*** K ] <sup>^^</sup></td><td>[[]]</td></tr>
+                    <tr></tr><td>Settlement Completed on 26 October 2024 [GMT+05:30] for 510.00 EUR to [HDFC *****308**6228 JIG**** PA*** K ] <sup>^^</sup></td><td>[[]]</td></tr>
+                    <tr></tr><td>EUR balance on 21 September 2024 [GMT+05:30]</td><td>1,300.00 EUR (+)</td></tr>
                 </table>
                 <hr/>
                 <div style="width: 100%;font-size: 10px;">
@@ -84,8 +86,7 @@ async function cleared()
     document.getElementById('mainDiv').innerHTML = `
     <div class="card bg-gradient-info">
     <h4><b>Current Settlement Summary:</b></h4>
-    Settlement Initiated : 25 September 2024 [GMT+05:30] for 510.00 EUR to [HDFC *****308**6228 JIG**** PA*** K ] <br>
-    Estimated Date :  10 November 2024 [GMT+05:30]<br><br>
+    --No Settlement Data --
     <div style="width: 100%;font-size: 10px;">
                     EUR to INR Settlement Terms:<br>
                    
